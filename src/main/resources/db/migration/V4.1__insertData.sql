dfc83d77c2e7a65b08f8047b1177113f069aa1cf
Yogesh Kumar Mangal <mangalyogesh550@gmail.com>
2022-09-08 15:33:22 +0530

    flyway demo

---
+++ src/main/resources/db/migration/V4.1__insertData.sql
@@ -0,0 +1,15 @@
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('yogesh', 'mangal', 'hello','a@gmail.com','8567838240');
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('ram', 'mangal', 'hello','a@gmail.com','8567838240');
+
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('shyam', 'mangal', 'hello','a@gmail.com','8567838240');
+
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('mohan', 'mangal', 'hello','a@gmail.com','8567838240');
+
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('radha', 'mangal', 'hello','a@gmail.com','8567838240');
+
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('dayal', 'mangal', 'hello','a@gmail.com','8567838240');
+
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('neha', 'mangal', 'hello','a@gmail.com','8567838240');
+
+INSERT INTO REGISTRATION_USERS(username, first_name, last_name,email,mobile) VALUES('sonu', 'mangal', 'hello','a@gmail.com','8567838240');
+
